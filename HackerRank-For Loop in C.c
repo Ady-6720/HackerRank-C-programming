d7bba2b3af2d135e497665d55240349c47ebc48c
Aditya Malode <82941123+Ady-6720@users.noreply.github.com>
2021-06-05 18:36:59 +0530

    Update HackerRank-For Loop in C.c

--- HackerRank-For Loop in C.c
+++ HackerRank-For Loop in C.c
@@ -2,34 +2,23 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-
-
-
 int main() 
-{   
-     const char car[][10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}; 
-    int num, a, b;
+{
+    int a, b;
     scanf("%d\n%d", &a, &b);
-  	if(a>=1 && b<=9){
-          for (num = a; num <= b; num++) {
-            printf("%s\n",car[num]);
-        }}
-    else if(a>=1 && b>9){
-        for (num = a; num <= 9; num++) {
-            printf("%s\n",car[num]);
-        }}
-    if(a%2==0){
-        printf("even\n");
+    char *str[] = {"one", "two", "three", "four", "five", "six", "seven",  "eight",  "nine", "even", "odd"};
+
+    scanf("%d\n%d", &a, &b);
+    for (int i=a; i<=b; i++)// i= 8 
+    {
+            if (i <= 9) 
+        {
+                printf ("%s\n", str[i-1]);
+        }
+            else
+        {
+                printf ("%s\n", str[9+(i%2)]);
+        }
     }
-    else{printf("odd");}
-    
-    if(b%2==0){
-        printf("even");
-    }
-    else{printf("odd\n");}
-    
-    
-    
     return 0;
 }
-
